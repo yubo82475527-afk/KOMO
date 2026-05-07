3b282f5da7463cb711586e59c6f2483e58daee13
yubo82475527-afk <yubo82475527-afk@users.noreply.github.com>
2026-05-07 07:12:56 +0000

    fix: 修复翻译文件导入路径

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -21,6 +21,6 @@ export default getRequestConfig(async () => {
 
   return {
     locale,
-    messages: (await import(`../messages/${locale}.json`)).default
+    messages: (await import(`../../messages/${locale}.json`)).default
   }
 })
